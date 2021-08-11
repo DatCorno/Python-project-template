5da476d436e4b1b71e86cfabf2747c5771bff2b5
François Corneau-Tremblay <corneau90@gmail.com>
2021-08-11 02:00:05 +0000

    Initial commit

---
+++ tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import os
+from invoke import task
+
+@task
+def lint(c):
+    """Pull Github's Superlinter image and run it against the current project."""
+    # Pull latest Superlinter image from repo.
+    c.run("docker pull github/super-linter:latest")
+    current_wd = os.getcwd()
+    c.run(f"docker run -e RUN_LOCAL=true -v {current_wd}:/tmp/lint github/super-linter")
+
+@task
+def test(c):
+    """Run the test suite."""
+    pass
