3f165c0554d39c7f17e200bd60b809d8d5781939
François Corneau-Tremblay <corneau90@gmail.com>
2021-08-12 01:22:53 +0000

    Add mypy to project dev dependencies

--- tasks.py
+++ tasks.py
@@ -11,6 +11,7 @@ def lint(c):
     c.run("docker pull github/super-linter:latest")
     current_wd = os.getcwd()
     c.run(f"docker run -e RUN_LOCAL=true -v {current_wd}:/tmp/lint github/super-linter")
+    c.run("mypy --strict")
 
 
 @task
